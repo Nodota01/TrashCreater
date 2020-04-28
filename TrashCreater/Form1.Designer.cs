cf766c62040fbf03f977a992ca98105ba036e070
Nodota <nodota01@outlook.com>
2020-04-28 22:53:19 +0800

    First version

---
+++ TrashCreater/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TrashCreater
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pathButton = new System.Windows.Forms.Button();
+            this.pathBox = new System.Windows.Forms.TextBox();
+            this.fileSizeBox = new System.Windows.Forms.TextBox();
+            this.generateButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // pathButton
+            // 
+            this.pathButton.Location = new System.Drawing.Point(349, 12);
+            this.pathButton.Name = "pathButton";
+            this.pathButton.Size = new System.Drawing.Size(75, 23);
+            this.pathButton.TabIndex = 0;
+            this.pathButton.Text = "选择路径";
+            this.pathButton.UseVisualStyleBackColor = true;
+            this.pathButton.Click += new System.EventHandler(this.pathButton_Click);
+            // 
+            // pathBox
+            // 
+            this.pathBox.Location = new System.Drawing.Point(12, 12);
+            this.pathBox.Name = "pathBox";
+            this.pathBox.ReadOnly = true;
+            this.pathBox.Size = new System.Drawing.Size(331, 21);
+            this.pathBox.TabIndex = 1;
+            // 
+            // fileSizeBox
+            // 
+            this.fileSizeBox.Location = new System.Drawing.Point(12, 39);
+            this.fileSizeBox.Name = "fileSizeBox";
+            this.fileSizeBox.Size = new System.Drawing.Size(100, 21);
+            this.fileSizeBox.TabIndex = 2;
+            // 
+            // generateButton
+            // 
+            this.generateButton.Location = new System.Drawing.Point(118, 39);
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(75, 23);
+            this.generateButton.TabIndex = 3;
+            this.generateButton.Text = "生成";
+            this.generateButton.UseVisualStyleBackColor = true;
+            this.generateButton.Click += new System.EventHandler(this.generateButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(447, 83);
+            this.Controls.Add(this.generateButton);
+            this.Controls.Add(this.fileSizeBox);
+            this.Controls.Add(this.pathBox);
+            this.Controls.Add(this.pathButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button pathButton;
+        private System.Windows.Forms.TextBox pathBox;
+        private System.Windows.Forms.TextBox fileSizeBox;
+        private System.Windows.Forms.Button generateButton;
+    }
+}
+
---
+++ TrashCreater/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrashCreater
+{
+    public partial class Form1 : Form
+    {
+
+        string folderPath;
+        string[] fileNames;
+        string[] fileSuffixes;
+        FileStream fileStream;
+
+        public Form1()
+        {
+            InitializeComponent();
+            fileNames = new string[] { "学习资料", "小电影", "我的照片", "神秘文件", "电子书", "学习笔记" };
+            fileSuffixes = new string[] {".jpg", ".pdf", ".exe", ".mp4", ".mp3", ".avi", ".docx", ".png" };
+        }
+
+        private void pathButton_Click(object sender, EventArgs e)
+        {
+            //选择文件
+            FolderBrowserDialog browserDialog = new FolderBrowserDialog();
+            if(browserDialog.ShowDialog() == DialogResult.OK){
+                Random random = new Random();
+                folderPath = browserDialog.SelectedPath;
+                pathBox.Text = folderPath;
+                string guid = Guid.NewGuid().ToString().Substring(0, 5);
+                string fullPath = folderPath + @"\" + fileNames[random.Next(0, fileNames.Length - 1)]
+                    + guid + fileSuffixes[random.Next(0, fileSuffixes.Length - 1)];
+                fileStream = new FileStream(fullPath, FileMode.OpenOrCreate, FileAccess.Write);
+            }
+        }
+
+        private void generateButton_Click(object sender, EventArgs e)
+        {
+            if(fileStream != null && Regex.IsMatch(fileSizeBox.Text, @"\d+"))
+            {
+                Random random = new Random();
+                int fileSize = Convert.ToInt32(fileSizeBox.Text);
+                int bufferSize = 1024 * 1024 * 50;
+                int haveWrite = 0;
+                byte[] buffer = new byte[bufferSize];
+                while(haveWrite < fileSize)
+                {
+                    random.NextBytes(buffer);
+                    //尚未写入大小
+                    if(fileSize - haveWrite < bufferSize)
+                    {
+                        fileStream.Write(buffer, 0, fileSize - haveWrite);
+                        haveWrite = fileSize;
+                    }
+                    else
+                    {
+                        fileStream.Write(buffer, 0, bufferSize);
+                        haveWrite += bufferSize;
+                    }
+                    fileStream.Flush();
+                }
+                MessageBox.Show("生成完成");
+                pathBox.Text = "";
+                fileStream.Dispose();
+                fileStream = null;
+            }
+            else
+            {
+                MessageBox.Show("尚未选择文件");
+            }
+        }
+    }
+}
